ace71c91cfb431b36ecb43df7aefd2ea730a894e
suman.pathak@smartims.com <suman.pathak@smartims.com>
2024-12-18 18:58:59 +0530

    evernorth 2nd round added

---
+++ criticalriverinc_2nd_round18.12.2024.py
@@ -0,0 +1,9 @@
+#Write 2 endpoints in Fastapi
+# 1. userlogin with authentiocation
+# 2. get dashboard
+# 3. add item
+
+
+#Design database for an e-commerce platform
+
+#Actual dIfferences between Django & FastAPI
