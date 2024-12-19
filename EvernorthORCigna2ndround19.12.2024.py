f9c8959365ec42c6b5c63705c8457a0e6fa6ce9a
suman.pathak@smartims.com <suman.pathak@smartims.com>
2024-12-19 17:12:44 +0530

    Evernorth

--- EvernorthORCigna2ndround19.12.2024.py
+++ EvernorthORCigna2ndround19.12.2024.py
@@ -1,6 +1,6 @@
 
 
-s = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+s = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 output = []
 index = 0
 group_size = 1
