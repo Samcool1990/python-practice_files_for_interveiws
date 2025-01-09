fe4110e29c79e8b58687ebc40b9206c502015ffc
suman.pathak@smartims.com <suman.pathak@smartims.com>
2025-01-09 11:52:40 +0530

    Cognizant Updated

---
+++ Cognizant.py
@@ -0,0 +1,17 @@
+# Stored procedure
+# Difference between function & stored procedure
+
+# console.log(2+5,"6");
+# ELastic Load Balancer
+# Docker
+# Kubernetes
+
+# what is the differenc between == & === in JS
+# The == operator performs a loose equality comparison that performs type coercion if necessary to make the 
+# comparison possible. The === operator, on the other hand, performs a strict equality comparison that does 
+# not perform type coercion and requires the operands to have the same type (as well as the same value).
+
+
+lst = [2,4,6]
+new_lst = [i+5 for i in lst]
+print(new_lst)
